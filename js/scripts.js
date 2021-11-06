3fcda4f4fe63be0caeca5c8dc337337ff70d8626
rserinas <rserinas@xtendly.com>
2021-11-06 15:53:16 +0800

    updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,43 @@
 $(function() {
     showCampaignSlogan();
+    initiateSlick();
 });
 
 function showCampaignSlogan() {
     $(".campaign-slogan").fadeIn(2500);
+}
+
+function initiateSlick() {
+    $('.responsive').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
 }
